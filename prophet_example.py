7cd6fd309041c30193b2761da10b4ef1814e8998
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-07-11 11:28:06 +0200

    added prophet, changes in run structure

--- prophet_example.py
+++ prophet_example.py
@@ -2,18 +2,19 @@ import neptune.new as neptune
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from pmdarima import auto_arima
+from prophet import Prophet
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
+from src import prophet_utils
 from src.feature_exgineering import prepare_features, exogenous_features
 
-np.random.seed(9473)
+np.random.seed(9476)
 path_to_data = "data/BAJAJFINSV.csv"
 stock_name = "BAJAJFINSV"
 
 # (neptune) create run
 run = neptune.init(project='common/project-time-series-forecasting',
-                   tags=['arimax'])
+                   tags=['prophet'])
 
 # (neptune) log stock name to the run
 run["info/stock_name"] = stock_name
@@ -43,19 +44,26 @@ run["valid/split"] = valid_date_split
 # (neptune) log exogenous feature names
 run["feature_engineering/exogenous_features"] = exogenous_features
 
-# fit arima
-model = auto_arima(
-    df_train.VWAP,
-    exogenous=df_train[exogenous_features],
-    trace=True,
-    error_action="ignore",
-    suppress_warnings=True
-)
-model.fit(df_train.VWAP, exogenous=df_train[exogenous_features])
-forecast = model.predict(n_periods=len(df_valid), exogenous=df_valid[exogenous_features])
-df_valid["Forecast_ARIMAX"] = forecast
+# fit Facebook Prophet model
+model = Prophet(daily_seasonality=True)
+for feature in exogenous_features:
+    model.add_regressor(feature)
 
-ax = df_valid[["VWAP", "Forecast_ARIMAX"]].plot(figsize=(9, 6))
+# (neptune) log model config
+prophet_utils.log_config(run, model)
+
+model.fit(df_train[["Date", "VWAP"] + exogenous_features].rename(columns={"Date": "ds", "VWAP": "y"}))
+
+# (neptune) log model params
+prophet_utils.log_params(run, model)
+
+forecast = model.predict(df_valid[["Date", "VWAP"] + exogenous_features].rename(columns={"Date": "ds"}))
+df_valid["Forecast_Prophet"] = forecast.yhat.values
+
+# (neptune) log forecast plots (can be interactive)
+prophet_utils.log_forecast_plots(run, model, forecast, log_interactive=True)
+
+ax = df_valid[["VWAP", "Forecast_Prophet"]].plot(figsize=(9, 6))
 ax.grid("both")
 
 # (neptune) log data and forecast as an interactive chart
@@ -63,5 +71,5 @@ run["visualizations/VMAP-forecast"] = neptune.types.File.as_html(ax.figure)
 plt.close("all")
 
 # (neptune) log final metrics
-run["valid/arimax/rmse"] = np.sqrt(mean_squared_error(df_valid.VWAP, df_valid.Forecast_ARIMAX))
-run["valid/arimax/mae"] = mean_absolute_error(df_valid.VWAP, df_valid.Forecast_ARIMAX)
+run["valid/prophet/rmse"] = np.sqrt(mean_squared_error(df_valid.VWAP, df_valid.Forecast_Prophet))
+run["valid/prophet/mae"] = mean_absolute_error(df_valid.VWAP, df_valid.Forecast_Prophet)
