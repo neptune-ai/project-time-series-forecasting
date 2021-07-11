a412fae45236fab25173175de616b26815e1c935
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-07-11 19:11:59 +0200

    added lstm

---
+++ lstm_example.py
@@ -0,0 +1,87 @@
+import glob
+
+import neptune.new as neptune
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from neptune.new.integrations.tensorflow_keras import NeptuneCallback
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+from src.feature_exgineering import prepare_data_for_lstm
+from src.model import get_model
+
+np.random.seed(9476)
+path_to_data = "data/BAJAJFINSV.csv"
+stock_name = "BAJAJFINSV"
+
+# (neptune) create run
+run = neptune.init(project="common/project-time-series-forecasting",
+                   tags=["lstm", "keras"])
+
+# (neptune) log stock name to the run
+run["info/stock_name"] = stock_name
+
+# load time series data (stock prices)
+df = pd.read_csv(path_to_data)
+df['Date'] = pd.to_datetime(df.Date, format='%Y-%m-%d')
+df.index = df['Date']
+
+ax = df.VWAP.plot(figsize=(9, 6))
+ax.grid("both")
+
+# (neptune) log VWAP chart as an interactive visualization
+run["visualizations/VWAP_chart"] = neptune.types.File.as_image(ax.figure)
+plt.close("all")
+
+# feature engineering and train/valid split
+valid_date_split = "2019"
+x_train, y_train, x_test, scaler, df_test = prepare_data_for_lstm(df, valid_date_split)
+
+# (neptune) log year for train/valid split
+run["valid/split"] = valid_date_split
+
+params = {
+    "loss": "mean_squared_error",
+    "optimizer": "adam",
+    "dropout": 0.2,
+    "lstm_units": 20,
+    "epochs": 2,
+    "batch_size": 64}
+
+# (neptune) log model params
+run["LSTM/params"] = params
+
+model = get_model(params=params, input_shape=x_train.shape[1])
+
+# (neptune) use TF-Keras integration to log training metrics
+neptune_callback = NeptuneCallback(run, base_namespace="training")
+
+model.fit(
+    x_train,
+    y_train,
+    epochs=params["epochs"],
+    batch_size=params["batch_size"],
+    verbose=1,
+    callbacks=[neptune_callback]
+)
+
+preds = model.predict(x_test)
+preds = scaler.inverse_transform(preds)
+df_test["Forecast_LSTM"] = preds
+
+ax = df_test[["VWAP", "Forecast_LSTM"]].plot(figsize=(9, 6))
+ax.grid("both")
+
+# (neptune) log data and forecast as an interactive chart
+run["visualizations/VWAP-forecast"] = neptune.types.File.as_html(ax.figure)
+plt.close("all")
+
+# (neptune) log final metrics
+run["valid/lstm/rmse"] = np.sqrt(mean_squared_error(df_test.VWAP, df_test.Forecast_LSTM))
+run["valid/lstm/mae"] = mean_absolute_error(df_test.VWAP, df_test.Forecast_LSTM)
+
+# (neptune) save model weights
+model.save('model_weights')
+run["LSTM/model_weights/saved_model.pb"].upload('model_weights/saved_model.pb')
+for name in glob.glob('model_weights/variables/*'):
+    run["LSTM/{}".format(name)].upload(name)
--- src/feature_exgineering.py
+++ src/feature_exgineering.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 
 exogenous_features = ["High_mean_lag3", "High_std_lag3", "Low_mean_lag3", "Low_std_lag3",
@@ -52,3 +53,48 @@ def prepare_features(df):
     df["day_of_week"] = df.Date.dt.dayofweek
 
     return df
+
+
+def prepare_data_for_lstm(df, valid_date_split):
+    features = ["Date", "VWAP"]
+    df_processed = df[features]
+
+    values = df_processed.VWAP.values
+    values = values.reshape(-1, 1)
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(values)
+
+    df_processed["VWAP"] = scaled_data
+
+    df_train = df_processed[df_processed.Date < valid_date_split]
+    df_test = df_processed[df_processed.Date >= valid_date_split]
+
+    df_train.index = df_train.Date
+    df_train.drop('Date', axis=1, inplace=True)
+
+    df_test.index = df_test.Date
+    df_test.drop('Date', axis=1, inplace=True)
+    df_test["VWAP"] = scaler.inverse_transform(df_test.values)
+
+    np_train = df_train.values
+    np_test = df_test.values
+
+    x_train, y_train = [], []
+    for i in range(90, len(np_train)):
+        x_train.append(scaled_data[i - 90:i, 0])
+        y_train.append(scaled_data[i, 0])
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    inputs = scaled_data[len(scaled_data) - len(np_test) - 90:]
+    x_test = []
+    for i in range(90, len(inputs)):
+        x_test.append(inputs[i - 90:i, 0])
+    x_test = np.array(x_test)
+
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    return x_train, y_train, x_test, scaler, df_test
+
