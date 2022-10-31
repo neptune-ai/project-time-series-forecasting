bba829dc8b77486d456db609b7a8cd7f2d64eaad
Prince Canuma <prince.gdt@gmail.com>
2022-10-31 16:30:16 +0100

    DL

---
+++ sales/DL/run_dl_lstm.py
@@ -0,0 +1,71 @@
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
+from pytorch_lightning.loggers import NeptuneLogger
+from neptune.new.types import File
+import pytorch_lightning as pl
+import seaborn as sns
+from data_module import *
+from model import *
+from utils import *
+
+
+def main():
+    params = {
+        "seq_len": 8,
+        "batch_size": 128,
+        "criterion": nn.MSELoss(),
+        "max_epochs": 1,
+        "n_features": 1,
+        "hidden_dim": 512,
+        "n_layers": 5,
+        "dropout": 0.2,
+        "learning_rate": 0.001,
+    }
+
+    # (neptune) Create NeptuneLogger instance
+    neptune_logger = NeptuneLogger()
+
+    early_stop = EarlyStopping(
+        monitor="val_loss", min_delta=1e-4, patience=1, verbose=False, mode="min"
+    )
+    lr_logger = LearningRateMonitor()
+
+    trainer = pl.Trainer(
+        max_epochs=params["max_epochs"],
+        callbacks=[early_stop, lr_logger],
+        logger=neptune_logger # neptune integration
+    )
+
+    dm = WalmartSalesDataModule(
+        seq_len=params["seq_len"], num_workers=8, path="./sales/data/aggregate_data.csv"
+    )
+
+    model = LSTMRegressor(
+        n_features=params["n_features"],
+        hidden_dim=params["hidden_dim"],
+        criterion=params["criterion"],
+        n_layers=params["n_layers"],
+        dropout=params["dropout"],
+        learning_rate=params["learning_rate"],
+    )
+
+    # Train model
+    trainer.fit(model, dm)
+
+    # Test model
+    test_loader = dm.test_dataloader()
+    predictions, values = model.predict(test_loader)
+    df_result = format_predictions(predictions, values, dm.df, dm.scaler)
+
+    preds_plot = sns.lineplot(data=df_result)
+
+     # (neptune) Log predictions visualizations
+    neptune_logger.experiment["training/plots/ypred_vs_y_valid"].upload(File.as_image(preds_plot.figure))
+
+    val_metrics = calculate_metrics(df_result)
+
+    # (neptune) Log scores
+    neptune_logger.experiment["training/val"] = val_metrics
+
+
+if __name__ == "__main__":
+    main()
---
+++ sales/DL/utils.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+def inverse_transform(scaler, df, columns):
+    for col in columns:
+        df[col] = scaler.inverse_transform(df[col])
+    return df
+
+
+def format_predictions(predictions, values, scaler):
+    vals = np.concatenate(values, axis=0).ravel()
+    preds = np.concatenate(predictions, axis=0).ravel()
+
+    df_result = pd.DataFrame(data={"value": vals, "prediction": preds})
+    df_result = df_result.sort_index()
+    df_result = inverse_transform(scaler, df_result, [["value", "prediction"]])
+    return df_result
+
+
+def calculate_metrics(df):
+    return {
+        "mae": mean_absolute_error(df.value, df.prediction),
+        "rmse": mean_squared_error(df.value, df.prediction) ** 0.5,
+        "r2": r2_score(df.value, df.prediction),
+    }
