d33bc1574cd9402fc3e005c2304f5c268f720887
Prince Canuma <prince.gdt@gmail.com>
2022-11-02 12:39:00 +0100

    add model registry

--- sales/DL/scripts/run_dl_lstm.py
+++ sales/DL/scripts/run_dl_lstm.py
@@ -1,6 +1,8 @@
 from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
 from pytorch_lightning.loggers import NeptuneLogger
+from neptune.new.exceptions import NeptuneException
 from neptune.new.types import File
+import neptune.new as neptune
 import pytorch_lightning as pl
 import seaborn as sns
 from data_module import *
@@ -32,7 +34,7 @@ def main():
     trainer = pl.Trainer(
         max_epochs=params["max_epochs"],
         callbacks=[early_stop, lr_logger],
-        logger=neptune_logger # neptune integration
+        logger=neptune_logger,  # neptune integration
     )
 
     dm = WalmartSalesDataModule(
@@ -58,13 +60,49 @@ def main():
 
     preds_plot = sns.lineplot(data=df_result)
 
-     # (neptune) Log predictions visualizations
-    neptune_logger.experiment["training/plots/ypred_vs_y_valid"].upload(File.as_image(preds_plot.figure))
+    # (neptune) Log predictions visualizations
+    neptune_logger.experiment["training/plots/ypred_vs_y_valid"].upload(
+        File.as_image(preds_plot.figure)
+    )
 
     val_metrics = calculate_metrics(df_result)
 
     # (neptune) Log scores
     neptune_logger.experiment["training/val"] = val_metrics
+
+    # (neptune) Initializing a Model and Model version
+    model_key = "DL"
+    project_key = neptune_logger.experiment["sys/id"].fetch().split("-")[0]
+
+    try:
+        model = neptune.init_model(key=model_key,)
+
+        print("Creating a new model version...")
+        model_version = neptune.init_model_version(model=f"{project_key}-{model_key}")
+
+    except NeptuneException:
+        print(
+            "A model with the provided key `{model_key}` already exists in this project."
+        )
+        print("Creating a new model version...")
+        model_version = neptune.init_model_version(model=f"{project_key}-{model_key}",)
+
+    # (neptune) Log run details
+    model_version["run/id"] = neptune_logger.experiment["sys/id"].fetch()
+    model_version["run/name"] = neptune_logger.experiment["sys/name"].fetch()
+    model_version["run/url"] = neptune_logger.experiment.get_url()
+
+    # (neptune) Log validation scores from run
+    model_version["training/val"] = neptune_logger.experiment["training/val"].fetch()
+
+    # (neptune) Download model checkpoint from Run
+    model_ckpt_name = get_model_ckpt_name(neptune_logger.experiment)
+    neptune_logger.experiment[f"training/model/checkpoints/{model_ckpt_name}"].download(
+        destination="."
+    )
+
+    # (neptune) Upload model checkpoint to Model registry
+    model_version["checkpoint"].upload(f"{model_ckpt_name}.ckpt")
 
 
 if __name__ == "__main__":
--- sales/DL/scripts/run_dl_lstm_finetune.py
+++ sales/DL/scripts/run_dl_lstm_finetune.py
@@ -1,6 +1,8 @@
 from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
 from pytorch_lightning.loggers import NeptuneLogger
+from neptune.new.exceptions import NeptuneException
 from neptune.new.types import File
+import neptune.new as neptune
 import pytorch_lightning as pl
 import seaborn as sns
 from data_module import *
@@ -22,10 +24,7 @@ def main():
     }
 
     # (neptune) Download model checkpoint from model registry
-    model_version = neptune.init_model_version(
-        model="TSF-DL",
-        with_id="TSF-DL-1",
-    )
+    model_version = neptune.init_model_version(with_id="TSF-DL-1",)
 
     model_version["checkpoint"].download()
 
@@ -40,7 +39,7 @@ def main():
     trainer = pl.Trainer(
         max_epochs=params["max_epochs"],
         callbacks=[early_stop, lr_logger],
-        logger=neptune_logger # neptune integration
+        logger=neptune_logger,  # neptune integration
     )
 
     dm = WalmartSalesDataModule(
@@ -56,7 +55,7 @@ def main():
         learning_rate=params["learning_rate"],
     )
 
-    model = model.load_from_checkpoint('checkpoint.ckpt')
+    model = model.load_from_checkpoint("checkpoint.ckpt")
 
     # Train model
     trainer.fit(model, dm)
@@ -68,13 +67,49 @@ def main():
 
     preds_plot = sns.lineplot(data=df_result)
 
-     # (neptune) Log predictions visualizations
-    neptune_logger.experiment["training/plots/ypred_vs_y_valid"].upload(File.as_image(preds_plot.figure))
+    # (neptune) Log predictions visualizations
+    neptune_logger.experiment["training/plots/ypred_vs_y_valid"].upload(
+        File.as_image(preds_plot.figure)
+    )
 
     val_metrics = calculate_metrics(df_result)
 
-    # (neptune) Log scores
+    # (neptune) Log validation scores
     neptune_logger.experiment["training/val"] = val_metrics
+
+    # (neptune) Initializing a Model and Model version
+    model_key = "DL"
+    project_key = neptune_logger.experiment["sys/id"].fetch().split("-")[0]
+
+    try:
+        model = neptune.init_model(key=model_key,)
+
+        print("Creating a new model version...")
+        model_version = neptune.init_model_version(model=f"{project_key}-{model_key}")
+
+    except NeptuneException:
+        print(
+            "A model with the provided key `{model_key}` already exists in this project."
+        )
+        print("Creating a new model version...")
+        model_version = neptune.init_model_version(model=f"{project_key}-{model_key}",)
+
+    # (neptune) Log run details
+    model_version["run/id"] = neptune_logger.experiment["sys/id"].fetch()
+    model_version["run/name"] = neptune_logger.experiment["sys/name"].fetch()
+    model_version["run/url"] = neptune_logger.experiment.get_url()
+
+    # (neptune) Log validation scores from run
+    model_version["training/val"] = neptune_logger.experiment["training/val"].fetch()
+
+    # (neptune) Download model checkpoint from Run
+    model_ckpt_name = get_model_ckpt_name(neptune_logger.experiment)
+    neptune_logger.experiment[f"training/model/checkpoints/{model_ckpt_name}"].download(
+        destination="."
+    )
+
+    # (neptune) Upload model checkpoint to Model registry
+    model_version["checkpoint"].upload(f"{model_ckpt_name}.ckpt")
 
 
 if __name__ == "__main__":
